aa10a821f2777b05df8cc940546e1c037ab70ca5
Bonnee <bonora.matteo@gmail.com>
2018-07-18 17:13:23 +0200

    Overhaul temp

--- temp.cpp
+++ temp.cpp
@@ -1,41 +1,30 @@
 #include "temp.h"
 
-Temp::Temp(byte attachTo)
-{
-	pin = attachTo;
-}
-
 void Temp::setup()
 {
 	sensor.setup(pin);
 	wait_start = millis();
 }
 
-int Temp::loop(unsigned long now)
+bool Temp::loop()
 {
-	if (now - wait_start >= sensor.getMinimumSamplingPeriod())
+	if (millis() - wait_start >= sensor.getMinimumSamplingPeriod())
 	{
-		if (sensor.getStatus() == 0)
-		{
-			wait_start = now;
-			double temp = sensor.getTemperature();
+		wait_start = millis();
 
-			/* Sometimes I get a checksum error from my DHT-22.
-         To avoid exceptions I check if the reported temp is a number.
-         This should work only with the "getStatus() == 0" above, but it gave me errors anyway, So I doublecheck */
-			if (!isnan(temp))
-			{
-				/*dtemp = round(temp * 2.0) / 2.0;
-			ctemp = round(temp);*/
-				return temp;
-			}
-		}
-		else
+		temp = sensor.getTemperature();
+		if (isnan(temp))
 		{
 			// If there's an error in the sensor, wait 5 seconds to let the communication reset
 			wait_start += 5000;
 			sensor.setup(pin);
 		}
+		return true;
 	}
-	return -1;
+	return false;
+}
+
+double Temp::get_temp()
+{
+	return temp;
 }
--- temp.h
+++ temp.h
@@ -1,16 +1,20 @@
 #ifndef TEMP_H
+#include <Arduino.h>
 #include <DHT.h> // https://github.com/markruys/arduino-DHT
 
 class Temp
 {
 	DHT sensor;
 	byte pin;
+	double temp;
 
 	unsigned long wait_start;
 
   public:
-	Temp(byte attachTo);
+	Temp(byte attach_to) : pin(attach_to) {}
 	void setup();
-	int loop(unsigned long now);
+	bool loop();
+
+	double get_temp();
 };
 #endif
